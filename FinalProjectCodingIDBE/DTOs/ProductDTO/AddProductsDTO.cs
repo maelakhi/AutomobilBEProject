68d732cb27f7c82af45a8fe73b82f77be354d317
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2023-12-16 16:41:56 +0700

    CRUD Product
    Add DTO, model, srvice, repository, controller

---
+++ FinalProjectCodingIDBE/DTOs/ProductDTO/AddProductsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalProjectCodingIDBE.DTOs.ProductDTO
+{
+    public class AddProductsDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public int IdCategory { get; set; }
+    }
+}
